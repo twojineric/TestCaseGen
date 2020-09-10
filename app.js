7b662069f974fde7c65fd3aba4c733ee78fb822a
Eric <57415580+twojineric@users.noreply.github.com>
2020-09-09 21:34:57 -0700

    Added support for String datatypes

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const button = document.getElementById('generate');
 const resetButton = document.getElementById('reset');
 const formData = document.getElementById('everything').elements;
 
-const output = document.querySelector(".result > p");
-
 main();
 
 function main()
 {
+    button.addEventListener('click', genList);
+    resetButton.addEventListener('click', hardReset);
+
     document.getElementById('datatype2').onchange = function() {
         document.getElementById('minLen').disabled = !this.checked;
         document.getElementById('maxLen').disabled = !this.checked;
@@ -17,14 +18,6 @@ function main()
         document.getElementById('maxRange').disabled = !this.checked;
         document.getElementById('decimals').disabled = !this.checked;
     };
-
-    button.addEventListener('click', () => {
-        genList();
-    });
-
-    resetButton.addEventListener('click', () => {
-        hardReset();
-    });
 }
 
 function genList()
@@ -33,6 +26,8 @@ function genList()
     let delimiter = formData.namedItem('separateC').value;
     let maxVal = parseInt(formData.namedItem('maxRange').value);
     let minVal = parseInt(formData.namedItem('minRange').value);
+    let maxLen = parseInt(formData.namedItem('maxLen').value);
+    let minLen = parseInt(formData.namedItem('minLen').value);
     let inputLength = parseInt(formData.namedItem('numbers').value);
 
     while(inputLength > 0 && elemList.length != inputLength)
@@ -40,24 +35,33 @@ function genList()
         if(formData.namedItem('datatype1').checked)
         {
             let num = Math.random() * (maxVal - minVal);
-            if(!formData.namedItem('decimals').value) {
-                num = Math.floor(num);
+            if(!formData.namedItem('decimals').value || formData.namedItem('decimals').value < 0) {
+                num = Math.floor(num) + minVal;
             }
             else {
-                num = num.toFixed(formData.namedItem('decimals').value);
+                num = (num + minVal).toFixed(formData.namedItem('decimals').value);
             }
-            num = minVal + num;
             elemList.push(num);
         }
         if(formData.namedItem('datatype2').checked)
         {
-            elemList = ["String things coming soon!"]; //soon!
-            break;
+            //A-Z is 65-90, a-z is 97-122
+            let strLen = Math.floor(Math.random() * (maxLen - minLen)) + minLen;
+            let str = '';
+            for(let i = 0; i < strLen; i++)
+            {
+                str = str + String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+            }
+            if(formData.namedItem('quote').checked) {
+                str = `"${str}"`;
+            }
+            elemList.push(str);
         }
     }
+    if(!delimiter) delimiter = ' ';
     let finalList = elemList.join(delimiter);
     finalList = formData.namedItem('startingC').value + finalList + formData.namedItem('endingC').value;
-    output.innerHTML = finalList;
+    document.getElementById('result').value = finalList;
 }
 
 function hardReset()
